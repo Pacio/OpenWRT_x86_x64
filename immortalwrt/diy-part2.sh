a327beb355ef34563f10331447e7f9755ae78935
Pacio <4995125@163.com>
2025-12-16 18:28:16 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
-sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
+sed -i 's/eth1/eth3/g' package/base-files/files/etc/board.d/99-default_network
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
